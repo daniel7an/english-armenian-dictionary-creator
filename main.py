c4e45cd882dc6514d0b52cf81b1a0df6bcb4d41b
magavuz77 <tatul.danielyan.s@gmail.com>
2022-03-18 16:38:37 +0400

    bug detected

--- main.py
+++ main.py
@@ -18,18 +18,21 @@ def process(words):
         meaning_txt = ''
         meaning = dictionary.meaning(word)
         translation = translator.translate(word, src='en', dest='hy').text
-        types = list(meaning.keys())
+        """A bug Error: AttributeError: 'NoneType' object has no attribute 'keys'"""
+        try:
+            types = list(meaning.keys())
 
-        for t in types:
-            spec_meaning = meaning[t][0].replace('"', "")
-            if types.index(t) == len(types) - 1:
-                meaning_txt += t + ' - ' + spec_meaning
-            else:
-                meaning_txt += t + ' - ' + spec_meaning + ', '
-        rows.append([word, meaning_txt, translation])
+            for t in types:
+                spec_meaning = meaning[t][0].replace('"',"")
+                if types.index(t) == len(types) - 1:
+                    meaning_txt += t + ' - ' + spec_meaning
+                else:
+                    meaning_txt += t + ' - ' + spec_meaning + ', '
+            rows.append([word, meaning_txt.rstrip('"'), translation])
+        except:
+            print(word)
     return rows
 
 if __name__ == '__main__':
     rows = process(words)
     convert_to_csv(rows)
-    
